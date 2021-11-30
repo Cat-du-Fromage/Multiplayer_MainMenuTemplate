a7b54cefa91029997718b8025432d5bab15ab821
Florian <florian.duruz@gmail.com>
2021-11-30 22:35:10 +0100

    a

--- Assets/0_Scripts/V2/Shared/ClientNetPortalV2.cs
+++ Assets/0_Scripts/V2/Shared/ClientNetPortalV2.cs
@@ -1,4 +1,6 @@
+using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace KaizerWaldCode.V2
 {
@@ -8,6 +10,23 @@ namespace KaizerWaldCode.V2
         private void Awake()
         {
             Instance = this;
+        }
+        
+        private static void ConnectClient(string playerName)
+        {
+            string clientGuid = ClientPrefs.GetGuid();
+            string payload = JsonUtility.ToJson(new ConnectionPayload()
+            {
+                clientGUID = clientGuid,
+                clientScene = SceneManager.GetActiveScene().buildIndex,
+                playerName = playerName
+            });
+
+            byte[] payloadBytes = System.Text.Encoding.UTF8.GetBytes(payload);
+
+            NetworkManager.Singleton.NetworkConfig.ConnectionData = payloadBytes;
+
+            NetworkManager.Singleton.StartClient();
         }
     }
 }
